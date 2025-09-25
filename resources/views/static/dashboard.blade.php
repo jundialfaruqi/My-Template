f800bf9f82aa77446920c1adcf8398c18112c048
jundialfaruqi <jundialfaruqi@gmail.com>
2025-09-25 16:23:38 +0700

    Sedikit perbaikan tampilan halaman login

--- resources/views/static/dashboard.blade.php
+++ resources/views/static/dashboard.blade.php
@@ -175,8 +175,8 @@
                 <div class="mt-auto hidden lg:block">
                     <button id="sidebar-toggle"
                         class="w-full flex items-center px-6 py-4 text-sm transition-all duration-300 rounded-b-xl">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="1.5" stroke="currentColor" class="h-5 w-5 mr-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                            stroke="currentColor" class="h-5 w-5 mr-2">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
                         </svg>
--- resources/views/static/login.blade.php
+++ resources/views/static/login.blade.php
@@ -8,62 +8,51 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="min-h-screen bg-base-200 flex items-center justify-center">
-    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-md border p-4 m-2">
-        <legend class="fieldset-legend gap-0.5">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="size-5">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M3 8.25V18a2.25 2.25 0 0 0 2.25 2.25h13.5A2.25 2.25 0 0 0 21 18V8.25m-18 0V6a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 6v2.25m-18 0h18M5.25 6h.008v.008H5.25V6ZM7.5 6h.008v.008H7.5V6Zm2.25 0h.008v.008H9.75V6Z" />
-            </svg>
-            Login
-        </legend>
-        <div class="card w-full max-w-md bg-base-100 shadow-xl">
+<body class="min-h-screen bg-base-200 flex items-center justify-center p-4">
+    <div class="card w-full max-w-md bg-base-100 shadow-xl">
+        <div class="card-body">
+            <h2 class="card-title text-2xl font-bold text-center justify-center mb-6">JWSM TV</h2>
 
-            <div class="card-body">
-                <h2 class="card-title text-2xl font-bold text-center justify-center mb-6">JWSM TV</h2>
+            <form>
+                <div class="form-control w-full px-4">
+                    <label class="label">
+                        <span class="label-text flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 text-primary" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                            </svg>
+                            Email
+                        </span>
+                    </label>
+                    <input type="email" placeholder="email@example.com" class="input input-bordered w-full" />
+                </div>
 
-                <form>
-                    <div class="form-control w-full px-4">
-                        <label class="label">
-                            <span class="label-text flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 text-primary" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                                </svg>
-                                Email
-                            </span>
-                        </label>
-                        <input type="email" placeholder="email@example.com" class="input input-bordered w-full" />
-                    </div>
+                <div class="form-control w-full mt-4 px-4">
+                    <label class="label">
+                        <span class="label-text flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 text-primary" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                            </svg>
+                            Password
+                        </span>
+                    </label>
+                    <input type="password" placeholder="Password" class="input input-bordered w-full" />
+                </div>
 
-                    <div class="form-control w-full mt-4 px-4">
-                        <label class="label">
-                            <span class="label-text flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 text-primary" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
-                                </svg>
-                                Password
-                            </span>
-                        </label>
-                        <input type="password" placeholder="Password" class="input input-bordered w-full" />
-                    </div>
+                <div class="form-control mt-5 px-4">
+                    <button class="btn btn-primary w-full">Login</button>
+                </div>
 
-                    <div class="form-control mt-5 px-4">
-                        <button class="btn btn-primary w-full">Login</button>
-                    </div>
-
-                    <div class="text-center mt-6">
-                        <p>Belum punya akun? <a href="#" class="link link-primary">Daftar</a></p>
-                    </div>
-                </form>
-            </div>
-
+                <div class="text-center mt-6">
+                    <p>Belum punya akun? <a href="#" class="link link-primary">Daftar</a></p>
+                </div>
+            </form>
         </div>
-    </fieldset>
+
+    </div>
 </body>
 
 </html>
